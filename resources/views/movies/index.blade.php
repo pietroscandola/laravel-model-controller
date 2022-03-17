99a673034bbcc039d4b7fc978ef2bf439886cc57
pietroscandola <pietro.scandolavr@gmail.com>
2022-03-17 16:59:23 +0100

    fix movies.index

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -4,15 +4,15 @@
 
 @section('content')
 <div class="container mt-5">
-  <div class="d-flex flex-wrap">
+  <div class="d-flex flex-wrap justify-content-center">
     @foreach($movies as $movie)
       <div class="card m-3" style="width: 18rem;">
           <div class="card-body">
             <h5 class="card-title">{{ $movie->title}}</h5>
             <h6 class="card-subtitle mb-2 text-muted">{{ $movie->original_title}}</h6>
-            <p class="card-text">{{ $movie->nationality}}</p>
-            <p class="card-text">{{ $movie->date}}</p>
-            <p class="card-text">Voto {{ $movie->vote}}</p>
+            <p class="card-text">Languages: {{ $movie->nationality}}</p>
+            <p class="card-text">Date: {{ $movie->date}}</p>
+            <p class="card-text">Vote: {{ $movie->vote}}</p>
             <a href="#" class="card-link">Scopri di più</a>      
           </div>
       </div>
