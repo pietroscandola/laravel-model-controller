de9aae1f397db494a9aebec23fcabccf3307adef
pietroscandola <pietro.scandolavr@gmail.com>
2022-03-17 17:35:01 +0100

    add button indietro in single card

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -3,11 +3,15 @@
 @section('title', $movie->title)
 
 @section('content')
-<div class="card-body">
-    <h5 class="card-title">{{ $movie->title}}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">{{ $movie->original_title}}</h6>
-    <p class="card-text">Languages: {{ $movie->nationality}}</p>
-    <p class="card-text">Date: {{ $movie->date}}</p>
-    <p class="card-text">Vote: {{ $movie->vote}}</p>        
+<div class="container mt-5">
+    <div class="card-body">
+        <h5 class="card-title">{{ $movie->title}}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">{{ $movie->original_title}}</h6>
+        <p class="card-text">Languages: {{ $movie->nationality}}</p>
+        <p class="card-text">Date: {{ $movie->date}}</p>
+        <p class="card-text">Vote: {{ $movie->vote}}</p>
+        <a href="{{route('movies.index')}}">Indietro</a>        
+    </div>
 </div>
+
 @endsection
